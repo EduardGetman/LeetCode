3bbe04f9ef89519bf3c7b170d52bb30f3a3466b6
Эдуард Гетман <getman.edik2016@yandex.ru>
2024-01-21 00:07:59 +0300

    35. Search Insert Position Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
    You must write an algorithm with O(log n) runtime complexity.

---
+++ Solutions.Test/SearchInsertPositionTest.cs
@@ -0,0 +1,144 @@
+﻿using Xunit;
+using Solutions.SearchInsertPosition;
+
+namespace Solutions.Test;
+
+public class SearchInsertPositionTest
+{
+    [Fact]
+    public void SearchInsertOfNonExistentTarget()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 1, 3, 6, 11, 14, 18, 32, 43, 59 };
+        int target = 23;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(6, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfExistentTarget()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 115;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(5, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItIsMoreLast()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 147;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(8, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItIsLessFirst()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 3;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(0, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItIsLast()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 137;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(7, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItIsFirst()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 5;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(0, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItLessLast()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 2, 4, 6 };
+        int target = 5;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(2, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfTargetWhenItMoreFirst()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        int[] nums = { 5, 12, 32, 43, 71, 115, 116, 137 };
+        int target = 6;
+        // Act
+        int resultIndex = solution.SearchInsert(nums, target);
+        // Assert
+        Assert.Equal(1, resultIndex);
+    }
+
+    [Fact]
+    public void SearchInsertOfRandom()
+    {
+        // Arrange
+        Solution solution = new Solution();
+        for (int i = 0; i < 10000; i++)
+        {
+            var list = new List<int>();
+            var lenght = Random.Shared.Next(1, 10);
+            for (int j = 0; j < lenght; j++)
+            {
+                list.Add(Random.Shared.Next(1, 10000));
+            }
+
+            int[] nums = list.Distinct().Order().ToArray();
+            int trueIndex = Random.Shared.Next(0, nums.Length);
+            int target = nums[trueIndex];
+            if (Random.Shared.Next(0, 2) == 1)
+            {
+                target++;
+                trueIndex++;
+            }
+            else  if (Random.Shared.Next(0, 2) == 1)
+            {
+                target--;
+            }
+            // Act
+            int resultIndex = solution.SearchInsert(nums, target);
+            // Assert
+            Assert.Equal(trueIndex, resultIndex);
+        }
+    }
+}
---
+++ Solutions/SearchInsertPosition/Solution.cs
@@ -0,0 +1,32 @@
+﻿namespace Solutions.SearchInsertPosition;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        var intervalStart = 0;
+        var intervalEnd = nums.Length - 1;
+        do
+        {
+            var index = (intervalEnd + intervalStart) / 2;
+
+            if (target == nums[index])
+            {
+                return index;
+            }
+            else if (target > nums[index])
+            {
+                intervalStart = index;
+            }
+            else
+            {
+                intervalEnd = index;
+            }
+
+        } while (intervalStart + 1 < intervalEnd);
+
+        return target <= nums[intervalStart] ? intervalStart 
+            : target <= nums[intervalEnd] ? intervalEnd 
+            : intervalEnd + 1;
+    }
+}
